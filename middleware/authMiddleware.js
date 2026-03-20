dca53c735bf1bb3f2241a2713a023716908b125b
Poornima <taehyungpurple02@gmail.com>
2026-03-20 14:46:19 +0530

    authenticate

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,4 +2,15 @@ const jwt=require('jsonwebtoken');
 
 module.exports=function (req,res,next){
     const token=req.header("Authorization")?.split(" ")[1];
+    if(!token){
+        return res.status(404).json({error:"No token"});
+    }
+
+    try {
+       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.id;
+    next();
+    } catch (error) {
+       res.status(401).json({ error: "Invalid token" }); 
+    }
 }
