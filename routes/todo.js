b7ece9a0b2f3b3f12f3004b06bcd93da3c3b3e20
Poornima <taehyungpurple02@gmail.com>
2026-03-25 09:26:45 +0530

    added subtask delete api

--- routes/todo.js
+++ routes/todo.js
@@ -47,10 +47,27 @@ router.put('/:id',auth,async(req,res)=>{
 
 router.delete("/:id",auth,async (req,res)=>{
   try{
-    await Todo.findByOneAndDelete({_id:req.params.id,user:req.user});
+    await Todo.findOneAndDelete({_id:req.params.id,user:req.user});
     res.json({message:"Deleted"});
   }catch(err){
     res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/:id/subtask/:subId",auth,async (req, res) => {
+  try {
+    const { id,subId } = req.params;
+    const todo = await Todo.findOne({ _id:id,user:req.user });
+    if (!todo) {
+      return res.status(404).json({error:"Todo not found" });
+    }
+    todo.subtasks=todo.subtasks.filter(
+      (sub)=>sub._id.toString() !== subId
+    );
+    await todo.save();
+    res.json(todo);
+  } catch (err) {
+    res.status(500).json({error: err.message });
   }
 });
 
